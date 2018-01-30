38f5882e8a23b1746e161f57fcb2fb86cedee09d
Sayre Thomas <sayrethomasis@gmail.com>
2018-01-30 11:11:51 -0700

    Add: Minimax Alogo

--- tictactoe/src/tictactoe/Main.java
+++ tictactoe/src/tictactoe/Main.java
@@ -1,48 +1,138 @@
 package tictactoe;
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+     
+    
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
         MainGame game = new MainGame();
-        nonPlayer opp = new nonPlayer();
+       // nonPlayer opp = new nonPlayer();
         game.initializeBoard();
+        
         System.out.println("Tic-Tac-Toe!");
         do
         {
             System.out.println("Current board layout:");
             game.printBoard();
-            int row;
-            int col;
+            int mark;
             do
             {
-                if(game.getCurrentPlayer() == 'x'){
-                    System.out.println("Player " + game.getCurrentPlayer() + ", enter an empty row and column to place your mark!");
-                    row = scan.nextInt()-1;
-                    col = scan.nextInt()-1;
+                if(game.getCurrentPlayer() == 'X'){
+                   // System.out.println("Player " + game.getCurrentPlayer() + ", enter an empty location 1-9!");
+                     mark = scan.nextInt()-1;
+                    //mark = minimax(game.getCurrentBoard(), 'X', 1000);
                 }
                 else{
-                    row = opp.getRow(game);
-                    col = opp.getCol();
+                    /*System.out.println("Player " + game.getCurrentPlayer() + ", enter an empty location 1-9!");
+                    mark = scan.nextInt()-1;*/
+                    //System.out.println(game.getCurrentPlayer());
+                    mark = minimax(game.getCurrentBoard(), 'O', 1000);
                 }
-                    
+                
             }
-            while (!game.placeMark(row, col));
+            
+            while (!game.placeMark(mark));{      
             game.changePlayer();
+            //System.out.println(minimax(game));
         }
-        while(!game.isWinner() && !game.isBoardFull());
-        if (game.isBoardFull() && !game.isWinner())
+        }
+        while(!game.isWinner(game.getCurrentPlayer()) && !game.isBoardFull());
+        if (game.isBoardFull() && !game.isWinner(game.getCurrentPlayer()))
         {
-            System.out.println("The game was a tie!");
             game.printBoard();
+            System.out.println("CATS GAME!");
         }
         else
         {
             System.out.println("Current board layout:");
             game.printBoard();
             game.changePlayer();
-            System.out.println(Character.toUpperCase(game.getCurrentPlayer()) + " Wins!");
+            System.out.println(game.getCurrentPlayer() + " IS THE WINNER!");
         }
     }
+
+    private static int victor(char[] newBoard, char player){
+        MainGame tempGame = new MainGame();
+        tempGame.makeBoard(newBoard);
+        char opp;
+        /*if(player == 'O') opp = 'X';
+        else opp = 'O';*/
+        if(tempGame.isWinner('O')) return 1;
+        else if(tempGame.isWinner('X')) return 2;
+        else return 0;      
+     }
+     private static int score(int point) {
+        if(point == 1) return -10;
+        else if(point == 2) return 10;
+        else return 0;
+    }
+    /* 
+    private static int adv(char[] board, char player) {
+        MainGame tempGame = new MainGame();
+        tempGame.makeBoard(board);
+        if(tempGame.blocker('O')) return 1;
+        else if(tempGame.blocker('X')) return 2;
+        return 0;
+    }
+    private static int heur(int point){
+        if(point == 1) return 10;
+        else if(point == 2) return -10;
+        else return 0;
+    }*/
+      private static int minimax(char[] board, char player, int depth){
+            int bestMove = 0;
+            int victor = victor(board, player);
+            if(victor != 0) return score(victor);
+            //int adv = adv(board, player);
+            //if(adv != 0) bestMove = heur(adv);
+            
+           if(depth != 0){
+            if(player == 'O'){ 
+                int min = Integer.MIN_VALUE;
+                for (int i = 0; i < board.length; i++){
+                   // System.out.println(i);
+                    if(board[i] != '-') continue;
+                    board[i] = player;
+                    //if(adv != 0) bestMove = heur(adv);
+                    int v = minimax(board, 'X', depth-1);
+                    if(v > min){
+                        min = v;
+                        bestMove = i;
+                    }
+                    board[i] = '-';
+                }
+                   return bestMove;
+                   
+            }
+            else {
+                int max = Integer.MAX_VALUE;
+                for (int i = 0; i < board.length; i++){
+                    if(board[i] != '-') continue;
+                    board[i] = player;
+                    
+                    int v = minimax(board, 'O', depth-1);
+                    //if(adv != 0) bestMove = heur(adv);
+                    
+                    if(v < max){
+                        max = v;
+                        bestMove = i;
+                    }
+                    
+                    board[i] = '-';
+                }
+                   
+                   return bestMove;
+                } 
+                   
+                }
+                return bestMove;
+            }
+
+    
 }
--- tictactoe/src/tictactoe/MainGame.java
+++ tictactoe/src/tictactoe/MainGame.java
@@ -1,12 +1,16 @@
 package tictactoe;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainGame {
 
-    private char[][] board;
+    private char[] board;
     private char currentPlayer;
 
     public MainGame() {
-        board = new char[3][3];
-        currentPlayer = 'x';
+        board = new char[9];
+        currentPlayer = 'X';
         initializeBoard();
     }
 
@@ -16,30 +20,37 @@ public class MainGame {
         return currentPlayer;
     }
 
+    public char[] getCurrentBoard()
+    {
+        return board;
+    }
 
     // Set/Reset the board back to all empty values.
     public void initializeBoard() {
 
-        // Loop through rows
-        for (int i = 0; i < 3; i++) {
-
-            // Loop through columns
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+        // Loop through array
+        for (int i = 0; i < 9; i++){
+                board[i] = '-';
             }
-        }
     }
+    public void makeBoard(char[] newBoard) {
 
+        // Loop through array
+        for (int i = 0; i < 9; i++){
+                board[i] = newBoard[i];
+            }
+    }
 
 
     // Print the current board (may be replaced by GUI implementation later)
     public void printBoard() {
         System.out.println("-------------");
-
+        int count = 0;
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
+                System.out.print(board[count] + " | ");
+                count++;
             }
             System.out.println();
             System.out.println("-------------");
@@ -52,26 +63,37 @@ public class MainGame {
     public boolean isBoardFull() {
         boolean isFull = true;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-') {
+        for (int i = 0; i < 9; i++) {
+                if (board[i] == '-') {
                     isFull = false;
                 }
-            }
         }
-
         return isFull;
     }
 
 
     // Returns true if there is a win, false otherwise.
     // This calls our other win check functions to check the entire board.
-    public boolean isWinner() {
-        return (checkRows() || checkColumns() || checkDiagonals());
+    public boolean isWinner(char play) {
+        char player;
+        if(play == 'X') player = 'O';
+        else player = 'X';
+        boolean isWin = false;
+        if(
+                (board[0] == player && board[1] == board[0] && board[2] == board[0]) ||
+                (board[3] == player && board[4] == board[3] && board[5] == board[3]) ||
+                (board[6] == player && board[7] == board[6] && board[8] == board[6]) ||
+                (board[0] == player && board[3] == board[0] && board[6] == board[0]) ||
+                (board[1] == player && board[4] == board[1] && board[7] == board[1]) ||
+                (board[2] == player && board[5] == board[2] && board[8] == board[2]) ||
+                (board[0] == player && board[4] == board[0] && board[8] == board[0]) ||
+                (board[2] == player && board[4] == board[2] && board[6] == board[2])
+                ) isWin = true;
+        return isWin;
     }
 
 
-    // Loop through rows and see if any are winners.
+    /*/ Loop through rows and see if any are winners.
     private boolean checkRows() {
         for (int i = 0; i < 3; i++) {
             if (checkRowCol(board[i][0], board[i][1], board[i][2]) == true) {
@@ -246,40 +268,91 @@ public class MainGame {
         }
         return location;
     }
-
+*/
 
     // Change player marks back and forth.
     public void changePlayer() {
-        if (currentPlayer == 'x') {
-            currentPlayer = 'o';
+        if (currentPlayer == 'X') {
+            currentPlayer = 'O';
         }
         else {
-            currentPlayer = 'x';
+            currentPlayer = 'X';
         }
     }
 
     // Places a mark at the cell specified by row and col with the mark of the current player.
-    public boolean placeMark(int row, int col) {
+    public boolean placeMark(int mark) {
 
         // Make sure that row and column are in bounds of the board.
-        if ((row >= 0) && (row < 3)) {
-            if ((col >= 0) && (col < 3)) {
-                if (board[row][col] == '-') {
-                    board[row][col] = currentPlayer;
+        if ((mark >= 0) && (mark < 9)) {
+                if (board[mark] == '-') {
+                    board[mark] = currentPlayer;
                     return true;
                 }
-            }
         }
-
         return false;
+    }
+    public void removeMark(int mark) {
+
+        board[mark] = '-';
     }
     
-    public boolean isMark(int row, int col){
-        if(board[row][col] != '-'){
-            return true;
+    public boolean isMark(int mark){
+        return board[mark] != '-';
+    }
+    
+    public ArrayList availableSpots(){
+        ArrayList<Integer> avail = new ArrayList<>();
+        for(int i = 0; i < 9; i++){
+            if(board[i] == '-'){
+                avail.add(i);
+            }
         }
+        //System.out.println(avail.size());
+        return avail;
+    }
+
+    boolean blocker(char opp) {
+        char player;
+        if(opp == 'X') player = 'O';
+        else player = 'X';
+        /*for(int i = 0; i < 7; i++){
+            for(int j = 1; j < 3; j++){
+                if(board[i] == player && board[i+j] == board[i])
+                    if(board[i+1] == opp || board[i+2] == opp) return true;
+                else if(board[i] == player && board[i+j] == board[i])
+                    if(i > 0 && board[i-1] == opp) return true;
+            }
+            
+        }*/
+        
+        if(board[0] == player && board[6] == player && board[3] == opp ||
+           board[1] == player && board[7] == player && board[4] == opp ||
+           board[2] == player && board[8] == player && board[5] == opp ||
+           board[3] == player && board[6] == player && board[0] == opp ||
+           board[4] == player && board[7] == player && board[1] == opp ||
+           board[5] == player && board[8] == player && board[2] == opp ||
+           board[0] == player && board[3] == player && board[6] == opp ||
+           board[1] == player && board[4] == player && board[7] == opp ||
+           board[2] == player && board[5] == player && board[8] == opp ||
+           board[0] == player && board[1] == player && board[2] == opp ||
+           board[3] == player && board[4] == player && board[5] == opp ||
+           board[6] == player && board[7] == player && board[8] == opp ||
+           board[1] == player && board[2] == player && board[0] == opp ||
+           board[4] == player && board[5] == player && board[3] == opp ||
+           board[7] == player && board[8] == player && board[6] == opp ||
+           board[0] == player && board[2] == player && board[1] == opp ||
+           board[3] == player && board[5] == player && board[4] == opp ||
+           board[6] == player && board[8] == player && board[7] == opp ||
+           board[0] == player && board[8] == player && board[4] == opp ||
+           board[6] == player && board[2] == player && board[4] == opp ||
+           board[0] == player && board[4] == player && board[8] == opp ||
+           board[4] == player && board[8] == player && board[0] == opp ||
+           board[6] == player && board[4] == player && board[2] == opp ||
+           board[2] == player && board[4] == player && board[6] == opp ) return true;
         return false;
     }
+
 }
 
 
